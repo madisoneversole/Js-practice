c71abda9817b03b44c53e6d04f8f32b31f43483f
Maddie Eversole <madisoneversole23@gmail.com>
2023-10-19 21:51:13 -0700

    first commit

---
+++ practice.js
@@ -0,0 +1,27 @@
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+
+
+let button = {
+    id: 1,
+    name: "Madison"
+}
+
+
+console.log(button);
+console.log("My name is: ",button.name);
+
+
+function doSomething(){
+    let functionVar = 10;
+    for(let i =0; i <10; i++){
+        console.log(i);
+    }
+}
+
+
+doSomething();
